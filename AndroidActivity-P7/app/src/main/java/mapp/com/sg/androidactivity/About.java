90aed265a4329a433d88e29c1b6bbc913c3b93f0
Nicholas <amperific@gmail.com>
2016-10-18 20:12:00 +0800

    Practical 5 & 6: Completed /
    Practical 7: Initialization
    
    Signed-off-by: Nicholas <amperific@gmail.com>

---
+++ AndroidActivity-P7/app/src/main/java/mapp/com/sg/androidactivity/About.java
@@ -0,0 +1,16 @@
+package mapp.com.sg.androidactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nixho on 18-Oct-16.
+ */
+
+public class About extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
